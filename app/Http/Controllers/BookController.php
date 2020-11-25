456cd5f77ed4bf12e2afaeb299f81153cd9517dd
Luiz Claudio <lcluizclaudio96@gmail.com>
2020-11-25 15:00:10 -0300

    detalhando produtos

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -16,78 +16,40 @@ class BookController extends Controller
         $this->objBook = new ModelBook();
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $book = $this->objBook->all();
         return view('index', compact('book'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $book=$this->objBook->find($id);
+        return view('show', compact('book'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
---
+++ resources/views/show.blade.php
@@ -0,0 +1,16 @@
+@extends('templates.template')
+@section('content')
+    <h1 class="text-center">Visualizar</h1>
+    <hr>
+    <div class="col-8 m-auto">
+        @php
+        $user = $book->find($book->id)->relUsers;
+        @endphp
+
+        Titulo:     {{$book->title}} <br>
+        paginas:    {{$book->pages}} <br>
+        preço:      {{$book->price}} <br>
+        Autor:      {{$user->name}} <br>
+        E-mail:     {{$user->email}} <br>
+    </div>
+@endsection
