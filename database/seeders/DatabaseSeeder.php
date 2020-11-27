af02fedb22408cfbd0e42751ced019c978eb4dee
Luiz Claudio <lcluizclaudio96@gmail.com>
2020-11-27 11:22:40 -0300

    seed de autor e de livro

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,13 +17,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $faker = Faker::create();
+        /*$faker2 = Faker::create();
+        foreach (range(1, 100) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker2->name,
+                'email' => $faker2->unique()->safeEmail,
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                ]);
+            }*/
+     $faker = Faker::create();
         foreach (range(1, 100) as $index) {
             DB::table('books')->insert([
-                'title' => $faker->name,
+                'title' => $faker->sentence,
                 'pages' => rand(100, 200),
                 'price' => rand(100, 200),
-                'id_user' => rand(1, 3)
+                'id_user' => rand(1, 100)
             ]);
         }
 
